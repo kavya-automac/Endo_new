f9b6b4b52bacdc442c92b1fa96743956a6c26c03
Vivek <iot.tcac@gmail.com>
2025-10-18 23:08:47 +0530

    changesv6

--- dev/dev_pro/views.py
+++ dev/dev_pro/views.py
@@ -577,8 +577,8 @@ def patient_save_report(request):
 
         # Local file paths
         # source_path = os.path.join(r'C:\Users\Vivek-Yoga\Downloads', str(pdf_file_path1))
-        # source_path = os.path.join(r'/home/pi/Downloads/', str(pdf_file_path1))
-        source_path = os.path.join(r'/home/lm-001/Downloads', str(pdf_file_path1))
+        source_path = os.path.join(r'/home/pi/Downloads/', str(pdf_file_path1))
+        # source_path = os.path.join(r'/home/lm-001/Downloads', str(pdf_file_path1))
         destination_path = os.path.join(settings.MEDIA_ROOT, 'reports', str(pdf_file_path1))
         # print('destination_path:', destination_path)
         # print('source_path:', source_path)
